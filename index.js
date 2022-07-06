2832586a008fe5e34872fe30f5b6282c4b982153
Kurt Vermillion <kurtv0727@gmail.com>
2022-07-06 18:13:58 -0400

    commit tree 289811e23fcd3a9d1ef2e8c3bc2a1a497ade8b86

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution here
+function findMatching(driverArray, driverName) {
+    // const newDriverArray = []
+    // for (const user of driverArray) {
+    //     if (user.toUpperCase() === driverName.toUpperCase()) {
+    //         newDriverArray.push(user)
+    //     }
+    // }
+    // return newDriverArray
+    return driverArray.filter((user) => {return user.toUpperCase() === driverName.toUpperCase()})
+}
+function fuzzyMatch(driverArray, driverName) {
+    const newDriverArray = []
+    for (const user of driverArray) {
+        if (user.charAt(0,1) === driverName.charAt(0,1)) {
+            newDriverArray.push(user)
+        }
+    }
+    return newDriverArray
+}
+function matchName(driverArray, driverName) {
+    const newDriverArray = []
+    for (const user of driverArray) {
+        if (user.name === driverName) {
+            newDriverArray.push(user)
+        }
+    }
+    return newDriverArray
+}
